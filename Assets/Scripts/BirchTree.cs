42e89ebcd1f6e1a75f62f9f69d09f96cac995558
Faizan Akheel <faizanakheel@Faizans-MacBook-Air.local>
2022-03-20 21:59:07 -0400

    Created a base for environment

---
+++ Assets/Scripts/BirchTree.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirchTree : MonoBehaviour
+{
+    public Transform prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Instantiate(prefab, new Vector3(Random.Range(-20.0f,20.0f), 0, Random.Range(-20.0f,20.0f)), Quaternion.identity);
+        }
+    }
+
+}
