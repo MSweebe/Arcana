8b55a2641d9b05439ea157575fc30f47ab61a840
faizan-akheel <fakheel@emich.edu>
2022-03-25 17:09:27 -0400

    Chnages to generation of random objects

--- Assets/Scripts/BirchTree.cs
+++ Assets/Scripts/BirchTree.cs
@@ -8,7 +8,7 @@ public class BirchTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 10; i++)
         {
             Instantiate(prefab, new Vector3(Random.Range(-20.0f,20.0f), 0, Random.Range(-20.0f,20.0f)), Quaternion.identity);
         }
--- Assets/Scripts/Rocks.cs
+++ Assets/Scripts/Rocks.cs
@@ -8,7 +8,7 @@ public class Rocks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i++)
         {
             Instantiate(prefab, new Vector3(Random.Range(-20.0f,20.0f), 0, Random.Range(-20.0f,20.0f)), Quaternion.identity);
         }
