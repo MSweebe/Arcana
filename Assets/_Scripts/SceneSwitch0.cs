c2e9677b046590130ef3b866b1d5873bcf4cb492
faizan-akheel <fakheel@emich.edu>
2022-03-27 17:37:20 -0400

    feat: Start Menu

---
+++ Assets/_Scripts/SceneSwitch0.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch0 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitFunction()
+    {
+        Application.Quit();
+    }
+}
