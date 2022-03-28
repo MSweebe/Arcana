0eed90fe783399f266c211c8898437d16060f649
MSweebe <msweebe@emich.edu>
2022-03-27 22:39:03 -0400

    final push

---
+++ Assets/_Scripts/DoorTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public Interactible interactible;
+    public GameObject door;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactible.onFire) 
+        {
+            door.SetActive(false);
+        }
+    }
+}
