1d3e5f34594d5d00daf530e4d8ed271f453ee6d7
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-04-13 11:14:29 -0400

    docs: add class docs
    - trigger and interactable with sub classes all have documentation

--- Assets/_Scripts/TriggerCollectible.cs
+++ Assets/_Scripts/TriggerCollectible.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A custom trigger class for picking up collectibles.
+/// </summary>
+/// <para>
+/// Basic action: when a interactible that points to a collectible is executed, the collectible appears.
+/// </para>
 public class TriggerCollectible : Trigger
 {
     public override void SetTrigger()
