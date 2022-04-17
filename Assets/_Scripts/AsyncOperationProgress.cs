68465d759525dd6feb16ccfd2151d2d860402d25
faizan-akheel <fakheel@emich.edu>
2022-04-17 13:24:14 -0400

    feat: Loading bar implemented

---
+++ Assets/_Scripts/AsyncOperationProgress.cs
@@ -0,0 +1,58 @@
+//This script lets you load a Scene asynchronously. It uses an asyncOperation to calculate the progress and outputs the current progress to Text (could also be used to make progress bars).
+
+//Attach this script to a GameObject
+//Create a Button (Create>UI>Button) and a Text GameObject (Create>UI>Text) and attach them both to the Inspector of your GameObject
+//In Play Mode, press your Button to load the Scene, and the Text changes depending on progress. Press the space key to activate the Scene.
+//Note: The progress may look like it goes straight to 100% if your Scene doesn’t have a lot to load.
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncOperationProgress : MonoBehaviour
+{
+    public Text m_Text;
+    public Button m_Button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Call the LoadButton() function when the user clicks this Button
+        m_Button.onClick.AddListener(LoadButton);
+        
+    }
+
+     void LoadButton()
+    {
+        //Start loading the Scene asynchronously and output the progress bar
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Level 0");
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Pro :" + asyncOperation.progress);
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                 //Activate the Scene
+                m_Text.text = "Loading progress: " + 100 + "%";
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
