d24beed0e466be4bfdeb6387bdccfbbcecfbd4aa
Olaoluwa Ife-Iji <oifeiji@emich.edu>
2022-04-19 19:05:16 -0400

    Feat: Added BGM for Levels

---
+++ Assets/_Scripts/BGMSelector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMSelector : MonoBehaviour
+{
+    public AudioSource[] BGM;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Scene currentScene = SceneManager.GetActiveScene ();
+        string sceneName = currentScene.name;
+
+        //Check to see what scene is loaded and assign correct BGM
+        if(sceneName == "Level_02")
+        {
+            BGM[0].Play();
+        }
+        else if(sceneName == "Level 0")
+        {
+            BGM[1].Play();
+        }
+        else
+        {
+            //do nothing
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
