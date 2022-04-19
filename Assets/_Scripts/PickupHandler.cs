b5c206d527b91a783ca1b3d9f297707fe43e980a
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-04-19 14:16:38 -0400

    bugfix:  remove UI shard from count

--- Assets/_Scripts/PickupHandler.cs
+++ Assets/_Scripts/PickupHandler.cs
@@ -15,6 +15,10 @@ public class PickupHandler : MonoBehaviour
     {
         //totalNum = GameObject.FindGameObjectsWithTag("Collectibles", true).Length;
         totalNum = GameObject.FindObjectsOfType<PickupFloat>(true).Length;
+
+        //to prevent it from including the UI shard
+        totalNum -= 1;
+
         // set count to zero
         count = 0;
         UpdateCount();
