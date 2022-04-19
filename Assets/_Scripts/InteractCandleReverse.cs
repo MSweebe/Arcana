d3631eb633a40a2b1894ca431ea97e577d348632
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-04-18 21:24:06 -0400

    Saving things so I still have work

---
+++ Assets/_Scripts/InteractCandleReverse.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An interactible class to add to candle in Unity
+/// </summary>
+/// <para>
+/// Must have a RigidBody attached, and Tags set to Environment_Int. 
+/// The candle object needs a child with a sprite renderer to be successful.
+/// The prefabs contain this.
+/// Layer is recommended to be set to Environment_Int.
+/// </para>
+public class InteractCandleReverse : Interactible
+{
+
+    public override void SetFire()
+    {
+        setSprite(true);
+        if (triggerObject != null)
+        {
+            GOTrigger.ResetTrigger();
+        }
+    }
+    public override void PutOutFire()
+    {
+        setSprite(false);
+        if (triggerObject != null)
+        {
+            GOTrigger.SetTrigger();
+        }
+    }
+
+    void setSprite(bool isActive)
+    {
+        GameObject child = this.gameObject.transform.GetChild(0).gameObject;
+        if (child != null)
+        {
+            SpriteRenderer sr = child.GetComponent<SpriteRenderer>();
+            sr.enabled = isActive;
+        }
+    }
+}
